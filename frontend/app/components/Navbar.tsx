a00d1d17b7a5e43b342718ee65b41eef14a03f52
Kaustubh <86054341+K-kaustubh420@users.noreply.github.com>
2026-01-22 12:55:11 +0000

    basic frontend

---
+++ frontend/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+  <a className="btn btn-ghost text-xl">Energy Anomaly Detection</a>
+  <p><b>lets</b> <span className="text-rotate">
+  <span className='bg-green-200'>
+    <span>save</span>
+    <span>conserve</span>
+    <span>detect wastage</span>
+    <span>optimize</span>
+    <span>for a better tommorow</span>
+  </span>
+</span></p>
+</div>
+  )
+}
+
+export default Navbar
